a5df9a9370f7e4d756e07ed415fc2ab323a3c88b
John Caraballo <jcaraballo019@gmail.com>
2023-11-25 19:26:47 -0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# AI-Agents
+
+A python based application that has a simple agent that can interface with an SQLite database and can be used with ChatGPT to respond to user queries
---
+++ main.py
@@ -0,0 +1,46 @@
+from langchain.chat_models import ChatOpenAI
+from langchain.prompts import (
+    ChatPromptTemplate,
+    HumanMessagePromptTemplate,
+    MessagesPlaceholder
+)
+from langchain.schema import SystemMessage
+from langchain.agents import OpenAIFunctionsAgent, AgentExecutor
+from dotenv import load_dotenv
+from tools.sql import list_tables, run_query_tool, describe_tables_tool
+
+load_dotenv()
+
+chat = ChatOpenAI()
+tables = list_tables()
+prompt = ChatPromptTemplate(
+    messages=[
+        SystemMessage(
+            content=(
+                "You are an AI that has access to a SQLite database.\n"
+                f"The database has tables of: {tables}\n"
+                "Do not make any assumptions about what tables exist "
+                "or what columns exist. Instead, use the 'describe_tables' function"
+            )
+        ),
+        HumanMessagePromptTemplate.from_template("{input}"),
+        MessagesPlaceholder(variable_name="agent_scratchpad")
+    ]
+)
+
+tools = [run_query_tool, describe_tables_tool]
+
+agent = OpenAIFunctionsAgent(
+    llm=chat,
+    prompt=prompt,
+    tools=tools
+)
+
+agent_executor = AgentExecutor(
+    agent=agent,
+    tools=tools,
+    verbose=True
+)
+
+
+agent_executor("How many users have provided a shipping address?")
